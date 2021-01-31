bed23e49fcb6e0644b63fee96079550b7297aa4c
Tom Firth <tom@fold.sh>
2021-01-31 23:20:24 +0000

    Validate project config and test
    Added in some tests for the project/service config logic. Naughty
    me for not doing it before!! Caught an issue with the validation
    logic doing it and tidied things up a bit in there as a result.

--- ctl/project/project.go
+++ ctl/project/project.go
@@ -38,54 +38,12 @@ func IsAFoldProject(path string) bool {
 	return true
 }
 
-// Looks for fold.yaml in the current directory and loads it.
 func Load(logger logging.Logger) (*Project, error) {
-	v := newViper()
-	var fileNotFound viper.ConfigFileNotFoundError
-	err := v.ReadInConfig()
-	if err != nil {
-		if errors.As(err, &fileNotFound) {
-			logger.Debugf("config file not found %v", err)
-			return nil, NotAFoldProject
-		} else {
-			logger.Debugf("config file invalid %v", err)
-			return nil, InvalidConfig
-		}
-	}
-	var p *Project
-	err = v.Unmarshal(&p)
-	if err != nil {
-		logger.Debugf("failed to unmarshal config %v", err)
-		return nil, InvalidConfig
-	}
-	p.logger = logger
-	for _, s := range p.Services {
-		s.project = p
-	}
-	return p, nil
+	return load(logger, ".")
 }
 
 func (p *Project) SaveConfig() error {
-	v := newViper()
-	v.Set("name", p.Name)
-	v.Set("maintainer", p.Maintainer)
-	v.Set("email", p.Email)
-	v.Set("repository", p.Repository)
-	v.Set("services", p.Services)
-	err := v.WriteConfigAs("./fold.yaml")
-	if err != nil {
-		fmt.Printf("%v", err)
-		return CantWriteConfig
-	}
-	return nil
-}
-
-func newViper() *viper.Viper {
-	v := viper.New()
-	v.AddConfigPath(".")
-	v.SetConfigName("fold")
-	v.SetConfigType("yaml")
-	return v
+	return saveConfig(p, ".")
 }
 
 func (p *Project) AddService(svc Service) {
@@ -103,4 +61,66 @@ func (p *Project) GetService(path string) (*Service, error) {
 		}
 	}
 	return nil, NotAService
+}
+
+// Looks for fold.yaml in the current directory and loads it.
+func load(logger logging.Logger, path string) (*Project, error) {
+	v := newViper(path)
+	var fileNotFound viper.ConfigFileNotFoundError
+	err := v.ReadInConfig()
+	if err != nil {
+		if errors.As(err, &fileNotFound) {
+			logger.Debugf("config file not found %v", err)
+			return nil, NotAFoldProject
+		} else {
+			logger.Debugf("config file invalid %v", err)
+			return nil, InvalidConfig
+		}
+	}
+	if !validateConfig(v) {
+		logger.Debugf("invalid config: must set name, maintainer, email and repository")
+		return nil, InvalidConfig
+	}
+	var p *Project
+	err = v.Unmarshal(&p)
+	if err != nil {
+		logger.Debugf("failed to unmarshal config %v", err)
+		return nil, InvalidConfig
+	}
+	p.logger = logger
+	for _, s := range p.Services {
+		s.project = p
+	}
+	return p, nil
+}
+
+func saveConfig(p *Project, path string) error {
+	v := newViper(path)
+	v.Set("name", p.Name)
+	v.Set("maintainer", p.Maintainer)
+	v.Set("email", p.Email)
+	v.Set("repository", p.Repository)
+	v.Set("services", p.Services)
+	err := v.WriteConfigAs(filepath.Join(path, "fold.yaml"))
+	if err != nil {
+		fmt.Printf("%v", err)
+		return CantWriteConfig
+	}
+	return nil
+}
+
+func newViper(path string) *viper.Viper {
+	v := viper.New()
+	v.AddConfigPath(path)
+	v.SetConfigName("fold")
+	v.SetConfigType("yaml")
+	return v
+}
+
+func validateConfig(v *viper.Viper) bool {
+	n := v.IsSet("name")
+	m := v.IsSet("maintainer")
+	e := v.IsSet("email")
+	r := v.IsSet("repository")
+	return n && m && e && r
 }
--- ctl/project/project_test.go
+++ ctl/project/project_test.go
@@ -1,11 +1,152 @@
 package project
 
-// TODO test load config
-// TODO test invalid config
-// TODO test write config does not exist
-// TODO test write config exists
-// TODO test add service
-// TODO test get service id
-// TODO test look up service ./path
-// TODO test look up service path
-// TODO test look up service non existant path
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"github.com/foldsh/fold/logging"
+)
+
+func TestProjectConfigValidation(t *testing.T) {
+	logger := logging.NewTestLogger()
+	dir, err := ioutil.TempDir("", "testProjectConfig")
+	if err != nil {
+		fmt.Printf("%+v", err)
+		t.Fatal("Failed to create temporary test directory")
+	}
+	defer os.RemoveAll(dir)
+	if IsAFoldProject(dir) {
+		t.Errorf("Empty directory should not be a valid fold project")
+	}
+	cfgPath := filepath.Join(dir, "fold.yaml")
+
+	_, err = load(logger, dir)
+	if !errors.Is(err, NotAFoldProject) {
+		t.Errorf("Empty directory should not be a valid fold project")
+	}
+
+	err = ioutil.WriteFile(cfgPath, []byte("not valid yaml"), 0644)
+	if err != nil {
+		t.Fatalf("Failed to write to config file.")
+	}
+	if !IsAFoldProject(dir) {
+		t.Errorf("After creating a fold.yaml file it should be a valid fold project")
+	}
+	_, err = load(logger, dir)
+	if !errors.Is(err, InvalidConfig) {
+		t.Errorf("Non yaml file should lead to an InvalidConfig error.")
+	}
+
+	err = ioutil.WriteFile(cfgPath, []byte("valid: yaml"), 0644)
+	if err != nil {
+		t.Fatalf("Failed to write to config file.")
+	}
+
+	cfg, err := load(logger, dir)
+	if !errors.Is(err, InvalidConfig) {
+		logger.Debugf("Loaded config: %+v", cfg)
+		t.Errorf("Expected InvalidConfig but got %v\n", err)
+	}
+
+	s := `name: blah
+maintainer: blah
+email: blah
+repository: blah`
+	err = ioutil.WriteFile(cfgPath, []byte(s), 0644)
+	if err != nil {
+		t.Fatalf("Failed to write to config file.")
+	}
+	cfg, err = load(logger, dir)
+	if err != nil {
+		t.Errorf("Expected to load config but got error %v", err)
+	}
+	expectation := &Project{
+		Name:       "blah",
+		Maintainer: "blah",
+		Email:      "blah",
+		Repository: "blah",
+	}
+	diffConfig(t, expectation, cfg)
+}
+
+func TestProjectLoadingAndSaving(t *testing.T) {
+	logger := logging.NewTestLogger()
+	dir, err := ioutil.TempDir("", "testProjectLoadAndSave")
+	if err != nil {
+		fmt.Printf("%+v", err)
+		t.Fatal("Failed to create temporary test directory")
+	}
+	defer os.RemoveAll(dir)
+
+	project := &Project{
+		Name:       "foo",
+		Maintainer: "tom",
+		Email:      "tom@tom.com",
+		Repository: "github.com/tom",
+		Services: []*Service{
+			&Service{Name: "a", Path: "a"},
+		},
+	}
+	saveConfig(project, dir)
+
+	loaded, err := load(logger, dir)
+	diffConfig(t, project, loaded)
+}
+
+func TestProjectServiceUtils(t *testing.T) {
+	project := &Project{
+		Name:       "foo",
+		Maintainer: "tom",
+		Email:      "tom@tom.com",
+		Repository: "github.com/tom",
+		Services:   []*Service{},
+		logger:     logging.NewTestLogger(),
+	}
+	project.AddService(Service{Name: "a", Path: "a"})
+	expectation := &Project{
+		Name:       "foo",
+		Maintainer: "tom",
+		Email:      "tom@tom.com",
+		Repository: "github.com/tom",
+		Services: []*Service{
+			&Service{Name: "a", Path: "a"},
+		},
+	}
+	diffConfig(t, expectation, project)
+
+	invalidServicePaths := []string{"./b", "b", "./b/", "./foo/a", ".a/a/"}
+	for _, p := range invalidServicePaths {
+		s, err := project.GetService(p)
+		if s != nil {
+			t.Errorf("Service should be nil when asking for an invalid service")
+		}
+		if !errors.Is(err, NotAService) {
+			t.Errorf("Expected NotAService but got %v", err)
+		}
+	}
+	validServicePaths := []string{"a", "./a", "a/", "./a/"}
+	for _, p := range validServicePaths {
+		s, err := project.GetService(p)
+		if err != nil {
+			t.Errorf("Expected error to be nil but found %v", err)
+		}
+		diffConfig(t, &Service{Name: "a", Path: "a"}, s)
+	}
+}
+
+func diffConfig(t *testing.T, expectation, actual interface{}) {
+	if diff := cmp.Diff(
+		expectation,
+		actual,
+		cmpopts.IgnoreUnexported(Service{}, Project{}),
+	); diff != "" {
+		t.Errorf("Expected loaded config to equal input (-want +got):\n%s", diff)
+	}
+}
